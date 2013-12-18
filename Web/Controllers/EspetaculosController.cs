dc5842a736553f358ba2736d3df8d07e00ae572b
andre agostinho <andre_ragostinho@hotmail.com>
2013-12-18 19:42:45 -0200

    private ActionResult removida. nao estava sendo utilizada

--- Web/Controllers/EspetaculosController.cs
+++ Web/Controllers/EspetaculosController.cs
@@ -34,10 +34,7 @@ namespace AgileTickets.Web.Controllers
             return RedirectToAction("Index");
         }
 
-        private ActionResult VoltaPraIndex()
-        {
-            return RedirectToAction("Index");
-        }
+       
 
     }
 }
