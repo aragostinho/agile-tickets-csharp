5891bf39fc7c0498ce6d135883dd2fd3d50f1ca7
andre agostinho <andre_ragostinho@hotmail.com>
2013-12-19 19:18:14 -0200

    Sugesto IDao

---
+++ Web/Repositorios/IDao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AgileTickets.Web.Repositorios
+{
+    public interface IDao<T, int>
+    {
+        IList<T> Todos();
+        T Selecione(T pObject);
+        void Atualiza(T pObject);
+        void Cadastra(T pObject);
+        void Apaga(T pObject);
+    }
+}
